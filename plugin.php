09fbfefb5a68ee138b0d5fb7dded928fca7b0ba0
carstingaxion <carstingaxion@users.noreply.github.com>
2024-05-30 15:47:46 +0000

    Update version-numbers to 0.3.23

--- plugin.php
+++ plugin.php
@@ -11,7 +11,7 @@
  * Plugin Name:       Label Printing
  * Plugin URI:        https://github.com/figuren-theater/label-printing
  * Description:       Create printable labels with blocks.
- * Version:           0.3.21
+ * Version:           0.3.23
  * Requires at least: 6.2
  * Requires PHP:      8.1
  * Author:            Carsten Bach
